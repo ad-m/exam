eb07acaa36809db01341b03b130feef1b8fb3edc
Adam Dobrawy <naczelnik@jawnosc.tk>
2022-02-05 06:54:23 +0100

    exercise-2: add example application

---
+++ exercise-2/exam_app/exam_app/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from exam_app.users.models import User
+from exam_app.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
