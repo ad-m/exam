915306882059bf8e147dcd84480fc333bdc991de
Adam Dobrawy <naczelnik@jawnosc.tk>
2022-02-11 17:22:46 +0100

    exercise-3: add aws cluster setup

---
+++ exercise-3/infra/helm.tf
@@ -0,0 +1,17 @@
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+      command     = "aws"
+    }
+  }
+}
+
+resource "helm_release" "exam" {
+  name       = "exam"
+
+  chart      = "./../helm/exam"
+}
